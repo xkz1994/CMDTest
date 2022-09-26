3ba3159175fdb2d0a9e3a5a80244a0dec236dbec
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-09-26 18:09:51 +0800

    添加项目文件。

---
+++ CMDTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+foreach (var i in Enumerable.Range(1, 30))
+{
+    Task.Run(() =>
+    {
+        var proc = new Process();
+        try
+        {
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            var dosLine = $"SystemSpeechTest.exe test{i} 我们test{i}";
+            proc.StandardInput.WriteLine(dosLine);
+            proc.StandardInput.WriteLine("exit");
+            while (!proc.HasExited)
+            {
+                proc.WaitForExit(1000);
+            }
+
+            var errorMsg = proc.StandardError.ReadToEnd();
+            proc.StandardError.Close();
+            if (string.IsNullOrEmpty(errorMsg) == false) throw new Exception(errorMsg);
+        }
+        finally
+        {
+            proc.Close();
+            proc.Dispose();
+        }
+
+        Console.WriteLine($"ok {i}");
+    });
+}
+
+Console.ReadKey();
